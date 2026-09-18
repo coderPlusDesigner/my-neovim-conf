e1e3d5f23d7f3e1d6b1335b045aa94bf0a5b7d24
Sherali <coderplusdesigner@gmail.com>
2026-09-18 14:50:12 +0500

    upt: background made transparent

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -5,6 +5,9 @@ return {
     priority = 1000,
     config = function()
       vim.cmd("colorscheme github_dark_default")
+      vim.cmd("highlight Normal guibg=none")
+      vim.cmd("highlight NormalFloat guibg=none")
+      vim.cmd("highlight SignColumn guibg=none")
     end,
   },
 }
